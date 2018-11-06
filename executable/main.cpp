186feeb968c053f5638ca1e028ca5b3c5665d3a0
Javier G. Sogo <jgsogo@gmail.com>
2018-11-06 18:26:06 +0100

    use modern CMake

--- executable/main.cpp
+++ executable/main.cpp
@@ -14,7 +14,15 @@ int main() {
     msg1.mutable_header()->set_recipient("library");
     msg1.set_content("main msg1");
     
-    auto response = conan_example::do_work(msg1);
-    std::cout << response.DebugString() << std::endl;
+    auto response1 = conan_example::do_work(msg1);
+    std::cout << response1.DebugString() << std::endl;
 
+    // Check the if/else inside the library
+    conan::Data msg2;
+    msg2.mutable_header()->set_sender("executable");
+    msg2.mutable_header()->set_recipient("otherlib");
+    msg2.set_content("main msg2");
+
+    auto response2 = conan_example::do_work(msg2);
+    std::cout << response2.DebugString() << std::endl;
 }
